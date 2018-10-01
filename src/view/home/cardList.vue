6bf3e9c0985a0cb7965b1738d1487f55743ad195
yangyihan <sd5352941@163.com>
2018-10-01 21:57:02 +0800

    .

--- src/view/home/cardList.vue
+++ src/view/home/cardList.vue
@@ -2,6 +2,9 @@
   <div class="card-box">
     <el-card class='card' v-for="item in 4" shadow="" :key="item">
       <img src="https://mmbiz.qpic.cn/mmbiz_jpg/7Jo2WybqIWlRyGDPGRFQXA68NN1rGEfRh0lo1D7KUPibPu2sJTvGTHa62QUCEMg6zicV8zkbrrbj80Gic6gOt5Glg/640?wx_fmt=jpeg&tp=webp&wxfrom=5&wx_lazy=1&wx_co=1" class="card_img">
+      <div>
+        <b>二环路-环球中心</b>
+      </div>
     </el-card>
   </div>
 </template>
@@ -25,16 +28,19 @@
     flex-direction:column;
   }
   .card {
-    height: 350px;
-    width: 500px;
+    height: 320px;
+    width: 450px;
     margin: 12px 0px;
   }
   .card:hover{
-    height: 450px;
+    height: 650px;
   }
 
   .card_img{
-    height: 300px;
+    height: 270px;
     width: 100%;
+  }
+  b {
+    font-size: 14px;
   }
 </style>
